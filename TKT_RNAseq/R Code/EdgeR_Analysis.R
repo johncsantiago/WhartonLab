39bf1a9685b54e7fbcd30bd86d3845a8dcb9013f
johncsantiago <61519162+johncsantiago@users.noreply.github.com>
2023-06-16 11:21:15 -0400

    Update

---
+++ TKT_RNAseq/R Code/EdgeR_Analysis.R
@@ -0,0 +1,38 @@
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("edgeR")
+
+library(edgeR)
+
+##Clean Trait Data and calculate CPMs
+countdata=read.csv("/Users/johncsantiago/Documents/TKT RNAseq/CountTables/TKT.counttable.csv",row.names=1)
+groups=read.csv("/Users/johncsantiago/Documents/TKT RNAseq/CountTables/TKT.metadata.csv",row.names=1)
+
+##normalize data
+countdata=countdata[, row.names(groups)]
+x <- countdata
+gr <- factor(groups$Groups)
+y <- DGEList(counts=x,group=gr)
+keep <- filterByExpr(y)
+y <- y[keep,,keep.lib.sizes=FALSE]
+z <- calcNormFactors(y, method = "TMM")
+##normalized cpm
+cpmdata=cpm(z)
+
+plotMDS(z, col=as.numeric(factor(gr)))
+
+##updated edger analysis pipeline
+design<-model.matrix(~0 + gr)
+colnames(design) = unique(gr)
+z = estimateDisp(z, design)
+fit <- glmQLFit(z, design)
+
+
+##compare = makeContrasts(Lactate, levels=design)
+##lrt = glmQLFTest(fit,contrast=as.vector(compare))
+
+##lrt <- glmLRT(fit,contrast=as.vector(compare))		
+##G_X_E<-topTags(lrt,adjust.method="BH",n = nrow(z$counts), sort.by="PValue")
+##AFLF=G_X_E$table
+##sigsAFLF=AFLF[AFLF$FDR<.05,]
