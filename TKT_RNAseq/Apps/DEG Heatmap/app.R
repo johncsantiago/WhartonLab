43daa462f1e3c9cc98bbc388424f9207eaba5c0f
johncsantiago <61519162+johncsantiago@users.noreply.github.com>
2023-06-17 19:01:43 -0400

    TKT Heatmap app

--- TKT_RNAseq/Apps/DEG Heatmap/app.R
+++ TKT_RNAseq/Apps/DEG Heatmap/app.R
@@ -8,12 +8,12 @@ if(length(new.packages)>0){
 library(shiny)
 library(ggfortify)
 library(heatmaply)
-library(plotly)
-library(colourpicker)
 
 git.dir = "https://raw.githubusercontent.com/johncsantiago/WhartonLab/master/TKT_RNAseq/CountTables/"
 cpmdata = read.csv(paste0(git.dir,"TKT_cpmdata.csv"),row.names = 1)
 groups  = read.csv(paste0(git.dir,"TKT.metadata.csv"),row.names = 1)
+TKT.EdgeR = read.csv(paste0(git.dir,"TKT.EdgeR.Table.csv"),row.names = 1)
+
 convert=read.csv("https://raw.githubusercontent.com/DavidRandLab/Santiago-et-al-2021-BMC-Genomics/main/Data%20Files/FBgnConversionTable.csv",row.names=1)
 temp=convert[,"Symbol"]
 temp2=setdiff(row.names(cpmdata),convert[,2])
@@ -21,113 +21,69 @@ names(temp)=convert[,2]
 names(temp2)=temp2
 convert=c(temp,temp2)
 
+comparison.groups = data.frame(GRF.CxDf  = c("^GR.F","Tkt..GR.F"),
+                               GRF.CxOE  = c("^GR.F", "Tkt..GR.F"),
+                               WTF.CxDf  = c("^WT.F", "Tkt..WT.F"),
+                               WTF.CxOE  = c("^WT.F", "Tkt..WT.F"),
+                               GRxWT.FC  = c("^GR.F", "^WT.F"),
+                               GRxWT.FDf = c("TktDfGR.F", "TktDfWT.F"),
+                               GRxWT.FOE = c("TktOEGR.F", "TktOEWT.F"),
+                               GRM.CxDf  = c("^GR.M","Tkt..GR.M"),
+                               GRM.CxOE  = c("^GR.M","Tkt..GR.M"),
+                               WTM.CxDf  = c("^WT.M", "Tkt..WT.M"),
+                               WTM.CxOE  = c("^WT.M", "Tkt..WT.M"),
+                               GRxWT.MC  = c("^GR.M", "^WT.M"),
+                               GRxWT.MDf = c("TktDfGR.M", "TktDfWT.M"),
+                               GRxWT.MOE = c("TktOEGR.M", "TktOEWT.M"))
+
+Titles = c("GR.FC x GR.FDf","GR.FC x GR.FOE",
+           "WT.FC x WT.FDf", "WT.FC x WT.FOE",
+           "GR.FC x WT.FC", "GR.FDf x WT.FDf", "GR.FOE x WT.FOE",
+           "GR.MC x GR.MDf", "GR.MC x GR.MOE",
+           "WT.MC x WT.MDf", "WT.MC x WT.MOE",
+           "GR.MC x WT.MC", "GR.MDf x WT.MDf", "GR.MOE x WT.MOE")
+
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
-  titlePanel("TKT RNAseq Experiment: Custom PCA Figure"),
+  titlePanel("TKT RNAseq Experiment: DEG Heatmaps"),
   sidebarLayout(position="left", sidebarPanel(
     
     fluidRow(
-      column(6,
-    checkboxGroupInput("genotype",
-                       h5("Genotype"),
-                       choices = list("G85R" = "GR",
-                                      "WT" = "WT"),
-                       selected = ""),
-      ),
-    
-    column(6,
-    checkboxGroupInput("sex",
-                       h5("Sex"),
-                       choices = list("Male"   = "M",
-                                      "Female" = "F"),
-                       selected = ""),
-    ),),
-    
-    
-    fluidRow(
-      column(6,
-    checkboxGroupInput("tkt",
-                       h5("TKT"),
-                       choices = list("Control" = "Control",
-                                      "Deficient"    = "DF",
-                                      "Over Expression" = "OE"),
-                       selected = ""),
-      ),
-    
-    column(6,
-           radioButtons("pc",
-                              h5("PC"),
-                              choices = list("PC1/2" = 1,
-                                             "PC1/3" = 2,
-                                             "PC2/3" = 3),
-                              selected = 1),
-    ),),
-    
+      column(8,
+             selectInput("comparison",
+                       h5("Comparison"),
+                       choices = list("GR.FC x GR.FDf"  = 1,
+                                      "GR.FC x GR.FOE"  = 2,
+                                      "WT.FC x WT.FDf"  = 3,
+                                      "WT.FC x WT.FOE"  = 4,
+                                      "GR.FC x WT.FC"   = 5,
+                                      "GR.FDf x WT.FDf" = 6,
+                                      "GR.FOE x WT.FOE" = 7,
+                                      "GR.MC x GR.MDf"  = 8,
+                                      "GR.MC x GR.MOE"  = 9,
+                                      "WT.MC x WT.MDf"  = 10,
+                                      "WT.MC x WT.MOE"  = 11,
+                                      "GR.MC x WT.MC"   = 12,
+                                      "GR.MDf x WT.MDf" = 13,
+                                      "GR.MOE x WT.MOE" = 14),
+                       selected = 1),
+      )),
     
     fluidRow(
-      column(6,
-             selectInput("color", h5("Color Variable"),
-                         choices = list("Genotype"   = "Genotype",
-                                        "Sex"       = "Sex",
-                                        "TKT" = "TKT"),
-                         selected="TKT"),
-    
-      ),
-    
-    
     column(6,
-           selectInput("shape", h5("Shape Variable"),
-                       choices = list("Genotype"   = "Genotype",
-                                      "Sex"       = "Sex",
-                                      "TKT" = "TKT"),
-                       selected="Genotype"),
+           numericInput("tophits",
+                        h5("Number of DE Genes"), 
+                        min = 10, 
+                        max = 13582,
+                        value = 25)
     ),),
-    
-    
-    fluidRow(
-      column(6,
-    sliderInput("size", h5("Size"), min = 0, max = 50, value = 15, step=1),
-      ),
-    
-    column(6,
-           sliderInput("tophits", h5("Number of PC Genes"), min = 10, max = 50, value = 25, step=5)
-    ),),
-    
-    selectInput("colselect", h5("Select Color"), 
-                choices = list("Choose Condition" = 0,
-                               "Color 1"            = 1, 
-                               "Color 2"             = 2,
-                               "Color 3"              = 3),
-                selected = 0),
-  
-  conditionalPanel(
-    condition = "input.colselect == 1",
-    colourInput("color1", "Color 1",
-                "brown",showColour = 'background')),
-  
-  
-  conditionalPanel(
-    condition = "input.colselect == 2",
-    colourInput("color2", "Color 2",
-                "lightskyblue",showColour = 'background')),
-  
-  conditionalPanel(
-    condition = "input.colselect == 3",
-    colourInput("color3", "Color 3",
-                "darkgoldenrod1",showColour = 'background')),
   
   width = 3),
     
     
     mainPanel(
-      plotlyOutput(outputId = "distPlot"),
-      fluidRow(
-        column(6,
-      plotlyOutput(outputId = "topPC1hits", height = 550),
-        ),
-      column(6,
-      plotlyOutput(outputId = "topPC2hits", height = 550))
-      ),),
+      plotlyOutput(outputId = "DEG.Heatmap", , height = 1000),
+      ),
   )
 )
 
@@ -135,202 +91,76 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
   
-  temp.groups=reactive({
-    use.groups =  groups
+  temp.data=reactive({
     
-    if(length(input$genotype) == 1){
-      use.groups = use.groups[use.groups$Genotype == input$genotype,]
-    }
+    use.samples =  comparison.groups[,as.numeric(input$comparison)]
+    use.samples = colnames(cpmdata)[c(grep(use.samples[1],colnames(cpmdata)),
+                                      grep(use.samples[2],colnames(cpmdata)))]
+    use.cpm = cpmdata[,use.samples]
     
-    if(length(input$sex) == 1){
-      use.groups = use.groups[use.groups$Sex == input$sex,]
-    }
+    use.FDR = TKT.EdgeR[,as.numeric(input$comparison)]
+    names(use.FDR) = row.names(TKT.EdgeR)
+    use.FDR = use.FDR[order(use.FDR)]
     
-    if(length(input$tkt) == 1){
-      use.groups = use.groups[use.groups$TKT == input$tkt,]
-    }
+    use.genes = use.FDR[1:as.numeric(input$tophits)]
     
-    if(length(input$tkt) == 2){
-      use.groups = use.groups[use.groups$TKT == input$tkt[1]|
-                                use.groups$TKT == input$tkt[2],]
-    }
-
-    return(use.groups)
-  })
-  
-  
-  newdata=reactive({
-    use.groups=temp.groups()
-    use.data   = cpmdata[,row.names(use.groups)]
-    use.data   = use.data[apply(use.data,1,sum)>0,]
-    pca <- prcomp(t(use.data), scale.=TRUE) 
-    gr <- as.data.frame(use.groups)
-    PC1=scale(pca$x[,1])
-    PC2=scale(pca$x[,2])
-    eigs <- pca$sdev^2
-    ve=signif(((eigs / sum(eigs))*100)[1:3],4)
-    pcs=matrix(c(1,2,1,3,2,3),ncol =3)
-    pcs=pcs[,as.numeric(input$pc)]
+    use.cpm = use.cpm[names(use.genes),]
     
-      
-    pca.data=data.frame(Sample=row.names(gr),
-                        ##PC1=scale(pca$x[,1]),
-                        ##PC2=scale(pca$x[,2]),
-                        PC1=scale(pca$x[,pcs[1]]),
-                        PC2=scale(pca$x[,pcs[2]]),
-                        sdev=pca$sdev,
-                        Genotype=gr$Genotype, 
-                        Sex=gr$Sex, 
-                        TKT=gr$TKT,
-                        Percent1=ve[pcs[1]],
-                        Percent2=ve[pcs[2]],
-                        Group=gr$Group)
+    use.data = cbind(use.genes, use.cpm)
     
-    return(pca.data)
+    return(use.data)
   })
 
-output$distPlot <- renderPlotly({
-  pca.data=newdata()
-  pca.shape = factor(pca.data[,input$shape])
-  pca.color = factor(pca.data[,input$color])
-  percent1 = pca.data$Percent1[1]
-  percent2 = pca.data$Percent2[1]
-  pcs=matrix(c(1,2,1,3,2,3),ncol =3)
-  pcs=pcs[,as.numeric(input$pc)]
-  
-  fig  = plot_ly(data = pca.data, 
-                 x = ~PC1, 
-                 y = ~PC2,
-                 name = ~Group,
-                 type = 'scatter',
-                 mode = 'markers',
-                 showlegend = T,
-                 color = pca.color,
-                 symbol = pca.shape,
-                 ##symbols = as.character(seq_along(unique(point.data$Set))),
-                 colors = c(input$color1, input$color2, input$color3)[c(1:length(levels(pca.color)))],
-                 marker = list(size = input$size,
-                               line = list(color = "black", width = 1.5)),
-                 hoverinfo = "text",
-                 hovertext = paste("Sample:", pca.data$Sample)) %>%
-    layout(xaxis = list(title = paste0("PC", pcs[1], "(", percent1, "%)")),
-           yaxis = list(title = paste0("PC", pcs[2], "(", percent2, "%)")))
-  
-  fig
-  
-    })
- 
-
-
-
-
-output$topPC1hits <- renderPlotly({
-  use.groups = temp.groups()
-  use.data   = cpmdata[,row.names(use.groups)]
-  use.data   = use.data[apply(use.data,1,sum)>0,]
-  pca.data <- prcomp(t(use.data), scale.=TRUE) 
-  pcs=matrix(c(1,2,1,3,2,3),ncol =3)
-  pcs=pcs[,as.numeric(input$pc)]
-  PCcontribute=pca.data$rotation[,pcs[1]]
-  PCcontribute=PCcontribute[order(PCcontribute,decreasing=T)]
-  
-  ##The column sum of squares of the loadings (pca$rotation) are the variances of PCs.
-  #the squared factor loading is the percent of variance in that variable explained by the factor
-  percents=(PCcontribute*PCcontribute)
-
-  ##puts the genes in decreasing order
-  percents=percents[order(percents,decreasing=T)]
-  pca.genes=PCcontribute[names(percents)[1:input$tophits]]
-
-  pca.genes=names(pca.genes[order(pca.genes)])
-  PCorder=pca.data$x[,pcs[1]]
-  PCorder=PCorder[order(PCorder)]
-  
-  top.pca=cpmdata[pca.genes,names(PCorder)]
-  row.names(top.pca)=
-    convert[row.names(top.pca)]
-  color.groups=groups[names(PCorder),]
 
   
-  col.text=1
-  if(ncol(top.pca)>18){
-    col.text=1-((ncol(top.pca)-18)*.011)
-  }
+output$DEG.Heatmap <- renderPlotly({
+  use.data = temp.data()
+  hmdata = as.matrix(use.data[,2:ncol(use.data)])
+  
+  ##col.text=1
+  ##if(ncol(top.pca)>18){
+    ##col.text=1-((ncol(top.pca)-18)*.011)
+  ##}
 
-  cpm.text = top.pca
+  color.groups = groups[colnames(hmdata),1:3]
+  
+  hover.text = round(hmdata, digits = 1)
+  
+  FDR = hmdata
+  FDR[,1:ncol(FDR)] = use.data[,1]
+  
+  FBgn = hmdata
+  FBgn[,1:ncol(FBgn)] = row.names(FBgn)
+  
   i=1
-  while(i<ncol(cpm.text)){
-    cpm.text[,i] = paste("CPM = ", round(cpm.text[,i], digits = 1))
+  while(i<ncol(hover.text)){
+    hover.text[,i] = paste("CPM = ", hover.text[,i],
+                           "\nFDR = ", FDR[,i],
+                           "\nFBgn = ", FBgn[,i])
     i=i+1
   }
-  
-  heatmaply(top.pca,trace="none",col=RdYlBu(100)[100:1], scale="row",
-            dendrogram = "none",Rowv=F,Colv=F,
-            cexRow = .75, na.color="grey",
-            labRow = row.names(top.pca),
-            cexCol = col.text,
+
+  heatmaply(hmdata,
+            trace="none",
+            col=RdYlBu(100)[100:1],
+            scale="row",
+            dendrogram = "both",
+            Rowv=T,
+            Colv=T,
+            cexRow = 1/(as.numeric(input$tophits)/25),
+            na.color="grey",
+            labRow = convert[row.names(hmdata)],
+            ##cexCol = col.text,
+            cexCol = 1,
             key = T,
             column_text_angle = 90,
-            col_side_colors=color.groups[,1:4],
+            col_side_colors=color.groups,
             col_side_palette=Spectral,
-            main=paste0("PC",pcs[1]),
-            custom_hovertext = cpm.text)
-})
-
-output$topPC2hits <- renderPlotly({
-  use.groups = temp.groups()
-  use.data   = cpmdata[,row.names(use.groups)]
-  use.data   = use.data[apply(use.data,1,sum)>0,]
-  pcs=matrix(c(1,2,1,3,2,3),ncol =3)
-  pcs=pcs[,as.numeric(input$pc)]
-  
-  pca.data <- prcomp(t(use.data), scale.=TRUE) 
-  PCcontribute=pca.data$rotation[,pcs[2]]
-  
-  PCcontribute=PCcontribute[order(PCcontribute,decreasing=T)]
-  
-  ##The column sum of squares of the loadings (pca$rotation) are the variances of PCs.
-  #the squared factor loading is the percent of variance in that variable explained by the factor
-  percents=(PCcontribute*PCcontribute)
-  
-  ##puts the genes in decreasing order
-  percents=percents[order(percents,decreasing=T)]
-  pca.genes=PCcontribute[names(percents)[1:input$tophits]]
-  
-  pca.genes=names(pca.genes[order(pca.genes)])
-  PCorder=pca.data$x[,pcs[2]]
-  PCorder=PCorder[order(PCorder)]
-  
-  top.pca=cpmdata[pca.genes,names(PCorder)]
-  row.names(top.pca)=
-    convert[row.names(top.pca)]
-  color.groups=groups[names(PCorder),]
-
-  col.text=1
-  if(ncol(top.pca)>18){
-    col.text=1-((ncol(top.pca)-18)*.011)
-  }
-  
-  cpm.text = top.pca
-  i=1
-  while(i<ncol(cpm.text)){
-    cpm.text[,i] = paste("CPM = ", round(cpm.text[,i], digits = 1))
-    i=i+1
-  }
-  
-  heatmaply(top.pca,trace="none",col=RdYlBu(100)[100:1], scale="row",
-            dendrogram = "none",Rowv=F,Colv=F,
-            cexRow = .75, na.color="grey",
-            cexCol = col.text,
-            labRow = row.names(top.pca),
-            key = T,
-            column_text_angle = 90,
-            col_side_colors=color.groups[,1:4],
-            col_side_palette=Spectral,
-            main=paste0("PC",pcs[2]),
-            custom_hovertext = cpm.text)
-  
-})
+            main=paste0("Top ",input$tophits, " DEGs for ", Titles[as.numeric(input$comparison)]),
+            custom_hovertext = hover.text)
+})##, function() {
+  ##height = as.numeric(input$tophits)
+##})
 
 }
 
